d59921964d419cb2949c8a9d7e1880d34820b77c
Austin Glaves <austinglaves@Austins-MacBook-Air.local>
2024-12-10 21:10:57 -0600

    Trained new model with better matches for players, refs, and ball.

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,6 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+# The models are too large to check into GitHub.  To get a new model,
+# run the training/football_training_yolo_v5.ipynb Jupyter Notebook.
+# If training takes too long locally, consider using Google Colabs.
+model = YOLO('models/best.pt')
 
 results = model.predict('input_videos/08fd33_4.mp4', save=True)
 print(results[0])
